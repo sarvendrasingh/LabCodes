e234bd67834bc42891ea7f97fbf45300890b05f0
Sarvendra Kumar Singh <sarvendrakumarsingh.singh4@gmail.com>
2018-11-21 01:07:27 +0530

    Add files via upload
    program to find the area of a given circle..

--- area.c
+++ area.c
@@ -1,16 +1,28 @@
-/* to find the area of a triangle using herons formula
-
+/*
+program to find the area of a cirle of given radius
 */
-
 #include<stdio.h>
-#include<math.h>
-void main()
+//#include<math.h>
+int main()
 {
-	float a, b, c, s, area, temp;
-	printf("ENTER THE SIDES OF TRIANGLE WHOSE AREA IS TO BE DETERMINED\n");
-	scanf("%f %f %f", &a, &b, &c );
-	s = (a + b + c) / 2.0f;
-	temp = s * (s-a) * (s-b) * (s-c);
-	area = sqrt(temp);
-	printf("THE AREA OF THE GIVEN TRIANGLE IS %.2f", area);
+	float r=0.0f, a=0.0f;
+	char ch;
+	do
+	{
+		printf("Enter the radius of circle to find its area:\n");
+		scanf("%f", &r);
+		a = 3.14159 * r * r ;
+		printf("The area of the circle of radius %f is %.4f\n", r, a);
+		printf("Do you wish to continue?\t [y/n]\n");
+		scanf(" %c ", &ch);
+	}while( ch == 'y' );
+	if (ch == 'n')
+	{
+		printf("Goodbye\n");
+	}
+	else 
+	{
+		printf("Sorry! Wrong input.\n");
+	}
+	return 0;
 }
