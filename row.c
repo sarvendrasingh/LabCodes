7ce7f4679c624eb4565fecbbb3e7c752ee59523b
Sarvendra Kumar Singh <sarvendrakumarsingh.singh4@gmail.com>
2018-12-04 13:43:44 +0530

    Create row.c
    Code to find row sum and column sum of a matrix

---
+++ row.c
@@ -0,0 +1,53 @@
+/* 
+    code to find the row sum and column sum of a matrix
+
+*/
+#include <stdio.h>
+int main()
+{
+    int i,j,a ,b, sum =0;
+    printf("Enter the order of matrix:\n");
+    scanf("%d%d", &a, &b);
+    int arr[a][b];
+    int arr1[a][b];
+    int arr2[a][b];
+    printf("Enter the values of matrix:\n");
+    for(i=0; i<a; i++)
+    {
+        for(j=0; j<b; j++)
+        {
+            //arr[i][j]++;
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+//    printf("Enter the row whose sum is to be calculated:\n");
+
+    printf("Sum of respective rows of given matrix are:\n");    
+    for (i = 0; i < a; i++)
+    {
+    for(j = 0; j<b ; j++)
+    {
+        sum += arr[i][j];
+        
+    }
+    printf("R%d = %d\n", (i+1), sum);
+    sum = 0;
+    }
+    printf("Sum of respective columns of given matrix are:\n");
+
+    for (i = 0; i < a; i++)
+    {
+    for(j = 0; j<b ; j++)
+    {
+        sum += arr[j][i];
+        
+    }
+    printf("C%d = %d\n", (i+1), sum);
+    sum = 0;
+    }
+   
+    printf("Enjoy !!\n");
+    
+    return 0;
+}
